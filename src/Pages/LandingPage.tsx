f64abdfcacefd75e42f69e91b9c2553c18a1fa23
hector <hojeda@pdx.edu>
2025-04-21 20:12:57 -0700

    Downgraded Vite and included Joyride as a dependency

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -53,7 +53,7 @@ function LandingPage({
   return (
     <>
       <Header onCreateVFE={onCreateVFE} onLoadTestVFE={onLoadTestVFE} />
-        <Stack sx={{ width: "78%", margin: "auto", padding: 2 }}>
+        <Stack sx={{ width: "75%", margin: "auto", padding: 2 }}>
           <Button 
             variant="outlined" 
             onClick={() => {
