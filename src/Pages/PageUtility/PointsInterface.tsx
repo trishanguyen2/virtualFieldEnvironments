88c1d2310c7114fee5b1a803e6f7677483a5eaa4
turrilld <166080327+turrilld@users.noreply.github.com>
2025-04-24 16:57:22 -0700

    initial points structures and storage.

---
+++ src/Pages/PageUtility/PointsInterface.tsx
@@ -0,0 +1,46 @@
+import localforage from "localforage";
+
+//Initialize Points
+export function InitializePoints() {
+  localforage
+    .setItem("Points", 0)
+    .then(() => {
+      console.log("Points initialized successfully!");
+    })
+    .catch((error) => {
+      console.error("Error storing data:", error);
+    });
+}
+
+//Increment Points by Amount
+export async function AddPoints(amount: number) {
+  let points = await localforage.getItem<number>("Points");
+
+  if (points != null) {
+    points = points + amount;
+  } else return;
+
+  localforage
+    .setItem("Points", points)
+    .then(() => {
+      console.log("Points updated successfully! New points are: " + points);
+      return;
+    })
+    .catch((error) => {
+      console.error("Error storing data:", error);
+    });
+}
+
+// Show current points
+export function PointsDisplay() {
+  //use effect for constant updates?
+
+  localforage
+    .getItem("Points")
+    .then((data) => {
+      console.log("Retrieved points:", data);
+    })
+    .catch((error) => {
+      console.error("Error retrieving data:", error);
+    });
+}
